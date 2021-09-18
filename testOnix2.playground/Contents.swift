54f3bb7d89f28ae7487312597242f89acccb67d7
alexsyvolap <80973788+alexsyvolap@users.noreply.github.com>
2021-09-18 18:14:33 +0300

    overload operators. prev commit has another way

--- testOnix2.playground/Contents.swift
+++ testOnix2.playground/Contents.swift
@@ -9,6 +9,31 @@ extension Vector: CustomStringConvertible {
     }
 }
 
+extension Vector {
+    static func + (left: Vector, right: Vector) -> Vector {
+        return Vector(x: left.x + right.x, y: left.y + right.y, z: left.z + right.z)
+    }
+    
+    static func - (left: Vector, right: Vector) -> Vector {
+        return Vector(x: left.x - right.x, y: left.y - right.y, z: left.z - right.z)
+    }
+    
+    static func * (left: Vector, right: Vector) -> Vector {
+        let newX = (left.y * right.z) - (left.z * right.y)
+        let newY = (left.z * right.x) - (left.x * right.z)
+        let newZ = (left.x * right.y) - (left.y * right.x)
+        return Vector(x: newX, y: newY, z: newZ)
+    }
+    
+    static func & (left: Vector, right: Vector) -> Double {
+        return (left.x * right.x) + (left.y * right.y) + (left.z * right.z)
+    }
+    
+    static func ^ (left: Vector, right: Vector) -> Double {
+        return (left & right) / (left.length() * right.length())
+    }
+}
+
 class Vector {
     var x = 0.0
     var y = 0.0
@@ -22,29 +47,6 @@ class Vector {
     
     func length() -> Double {
         return sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
-    }
-    
-    func scalar(v: Vector) -> Double {
-        return (self.x * v.x) + (self.y * v.y) + (self.z * v.z)
-    }
-    
-    func product(v: Vector) -> Vector {
-        let newX = (self.y * v.z) - (self.z * v.y)
-        let newY = (self.z * v.x) - (self.x * v.z)
-        let newZ = (self.x * v.y) - (self.y * v.x)
-        return Vector(x: newX, y: newY, z: newZ)
-    }
-    
-    func angle(v: Vector) -> Double {
-        return self.scalar(v: v) / (self.length() * v.length())
-    }
-    
-    func sum(v: Vector) -> Vector {
-        return Vector(x: self.x + v.x, y: self.y + v.y, z: self.z + v.z)
-    }
-    
-    func diff(v: Vector) -> Vector {
-        return Vector(x: self.x - v.x, y: self.y - v.y, z: self.z - v.z)
     }
     
     static func getVectors(N: Int) -> Array<Vector> {
@@ -70,13 +72,15 @@ var vectors = Vector.getVectors(N: 2)
 print("vectors:", vectors)
 var v1 = vectors.first!
 var v2 = vectors.last!
+print("v1=", v1)
+print("v2=", v2)
 
 print("v1 length:", v1.length())
 print("v2 length:", v2.length())
-print("scalar:", v1.scalar(v: v2))
-print("product v1:", v1.product(v: v2))
-print("product v2:", v2.product(v: v1))
-print("angle:", v1.angle(v: v2))
-print("sum:", v1.sum(v: v2))
-print("diff v1-v2:", v1.diff(v: v2))
-print("diff v2-v1:", v2.diff(v: v1))
+print("scalar:", v1 & v2)
+print("product v1:", v1 * v2)
+print("product v2:", v2 * v1)
+print("angle:", v1 ^ v2)
+print("sum:", v1 + v2)
+print("diff v1-v2:", v1 - v2)
+print("diff v2-v1:", v2 - v1)
